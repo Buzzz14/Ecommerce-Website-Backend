6dcb5c0b0e60a4c6d7ec25b485655e31794aa4a1
Buzzz14 <sushanbajracharya14@gmail.com>
2024-09-28 00:32:13 +0545

    Finished User Authentication Code.

--- src/main/java/com/buzz/EcommerceWebsiteApplication.java
+++ src/main/java/com/buzz/EcommerceWebsiteApplication.java
@@ -1,4 +1,4 @@
-package ecommerce_website;
+package com.buzz;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/main/java/com/buzz/config/JwtConstant.java
+++ src/main/java/com/buzz/config/JwtConstant.java
@@ -1,6 +1,6 @@
 package com.buzz.config;
 
 public class JwtConstant {
-    public static final String SECRET_KEY= "dsdafweuifhfsdfasdf";
+    public static final String SECRET_KEY= "dsdafweuifhfsdfasdfeweadeaweawefsfewfewfsfefefes";
     public static final String JWT_HEADER= "Authorization";
 }
